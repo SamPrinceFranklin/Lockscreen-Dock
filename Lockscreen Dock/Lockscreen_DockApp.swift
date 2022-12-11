009d914d5cea41ed0b70b1deca46b39435f66284
samprincefranklin <samprince.franklink2020@vitstudent.ac.in>
2022-12-11 10:25:42 +0530

    Initial Commit

---
+++ Lockscreen Dock/Lockscreen_DockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lockscreen_DockApp.swift
+//  Lockscreen Dock
+//
+//  Created by Sam Prince K on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Lockscreen_DockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
